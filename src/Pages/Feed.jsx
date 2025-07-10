40e404982d57baa892233f9da0b0cbd542ac3cf8
abhishek1709-vesit <abhishekgore1706@gmail.com>
2025-07-10 22:05:34 +0530

    Changes in feed

--- src/Pages/Feed.jsx
+++ src/Pages/Feed.jsx
@@ -1,10 +1,11 @@
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { Stats } from "../Components/Stats";
 import { ThoughtCard } from "../Components/ThoughtCard";
 import { useGetLocalInfo } from "../Hooks/useGetLocalInfo";
 import { useGetThoughts } from "../Hooks/useGetThoughts";
 import { useEffect } from "react";
 import { Loader } from "../Components/Loader";
+import { FaEdit } from "react-icons/fa";
 
 export const Feed = () => {
   const { thoughtArray, loading } = useGetThoughts();
@@ -15,6 +16,9 @@ export const Feed = () => {
     <div className="my-10 bg-gradient-to-r from-black via-gray-900 to-black">
         <div>
           <Stats />
+        </div>
+        <div>
+          <button className="fixed top-30 right-2 md:right-10 p-2 "><NavLink to={'/new-thought'} className="flex items-center"><FaEdit className="font-bold text-4xl "/><span className="hidden md:inline">Add Your Thought</span></NavLink></button>
         </div>
         <ul className="flex items-center justify-center flex-col gap-10 mt-10 ">
           {thoughtArray.map((curThought) => {
